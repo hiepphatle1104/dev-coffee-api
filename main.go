d6b170d72ce36bf49f98ddd8d2fb50d71110eaa5
hiepphatle1104 <phathieple1104@gmail.com>
2025-05-13 21:58:38 +0700

    Adding delete, update order feature and fixing create order feature

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"dev-coffee-api/modules/drinks/model"
 	"dev-coffee-api/modules/drinks/transport/drinkRouter"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
@@ -8,6 +9,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 )
 
 //type OrderStatus int
@@ -18,22 +21,60 @@ import (
 //	OrderStatusCancelled
 //)
 
-type Orders struct {
-	ID           int    `json:"id" gorm:"column:id;"`
-	CustomerName string `json:"customer_name" gorm:"column:customer_name;"`
-	TotalPrice   int    `json:"total_price" gorm:"column:total_price;"`
-	Status       string `json:"status" gorm:"column:status;"`
-	CreatedAt    string `json:"created_at" gorm:"column:created_at;"`
-	UpdatedAt    string `json:"updated_at" gorm:"column:updated_at;"`
+type Order struct {
+	ID           int         `json:"id" gorm:"column:id;"`
+	CustomerName string      `json:"customer_name" gorm:"column:customer_name;"`
+	Status       string      `json:"status" gorm:"column:status;"`
+	OrderItems   []OrderItem `json:"order_items" gorm:"-"`
+	CreatedAt    *time.Time  `json:"created_at" gorm:"column:created_at;"`
+	UpdatedAt    *time.Time  `json:"updated_at" gorm:"column:updated_at;"`
 }
+
+func (Order) TableName() string { return "orders" }
 
 type OrderItem struct {
+	OrderID  int `json:"-" gorm:"column:order_id;"`
+	ItemID   int `json:"item_id" gorm:"column:item_id;"`
+	Quantity int `json:"quantity" gorm:"column:quantity;"`
 }
 
+func (OrderItem) TableName() string { return "order_items" }
+
 type OrderCreation struct {
-	ID           int    `json:"-" gorm:"column:id;"`
-	CustomerName string `json:"customer_name" gorm:"column:customer_name;"`
+	ID           int         `json:"-" gorm:"column:id;"`
+	CustomerName string      `json:"customer_name" gorm:"column:customer_name;"`
+	OrderItems   []OrderItem `json:"order_items" gorm:"-"`
 }
+
+func (OrderCreation) TableName() string { return Order{}.TableName() }
+
+type OrderUpdate struct {
+	ID           int         `json:"-" gorm:"column:id;"`
+	CustomerName string      `json:"customer_name" gorm:"column:customer_name;"`
+	OrderItems   []OrderItem `json:"order_items" gorm:"-"`
+	Status       string      `json:"status" gorm:"column:status;"`
+}
+
+func (OrderUpdate) TableName() string { return Order{}.TableName() }
+
+type Payment struct {
+	ID      int        `json:"id" gorm:"column:id;"`
+	OrderID int        `json:"order_id" gorm:"column:order_id;"`
+	Method  string     `json:"method" gorm:"column:method;"`
+	PaidAt  *time.Time `json:"paid_at" gorm:"column:paid_at;"`
+	Amount  float64    `json:"amount" gorm:"column:amount;"`
+}
+
+func (Payment) TableName() string { return "payments" }
+
+type PaymentCreation struct {
+	ID      int     `json:"-" gorm:"column:id;"`
+	OrderID int     `json:"order_id" gorm:"column:order_id;"`
+	Method  string  `json:"method" gorm:"column:method;"`
+	Amount  float64 `json:"amount" gorm:"column:amount;"`
+}
+
+func (PaymentCreation) TableName() string { return Payment{}.TableName() }
 
 func main() {
 	dsn, ok := os.LookupEnv("DB_CONN_URL")
@@ -60,9 +101,9 @@ func main() {
 	{
 		order.GET("", ListOrders(db))
 		order.POST("", CreateOrder(db))
-		order.GET("/:id")
-		order.PATCH("/:id")
-		order.DELETE("/:id")
+		order.GET("/:id", GetOrderByID(db))
+		order.PATCH("/:id", UpdateOrder(db))
+		order.DELETE("/:id", DeleteOrder(db))
 	}
 
 	if err := router.Run(":" + port); err != nil {
@@ -70,11 +111,85 @@ func main() {
 	}
 }
 
+func UpdateOrder(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var data OrderUpdate
+		if err = c.ShouldBind(&data); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Check item is valid
+		for _, orderItem := range data.OrderItems {
+			var item model.DrinkItem
+			if err = db.Table(model.DrinkItem{}.TableName()).First(&item, orderItem.ItemID).Error; err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": err.Error(),
+					"data":  orderItem.ItemID,
+				})
+
+				return
+			}
+		}
+
+		// Update
+		if err = db.Where("id = ?", id).Updates(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// TODO: Update item
+
+		c.JSON(http.StatusOK, gin.H{"data": true})
+	}
+}
+
+func DeleteOrder(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err = db.Table(Order{}.TableName()).Where("id = ?", id).Delete(nil).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"data": true})
+	}
+}
+
+func GetOrderByID(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var data Order
+		if err = db.Where("id = ?", id).First(&data).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"data": data})
+	}
+}
+
 func CreateOrder(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var order Orders
+		var data OrderCreation
 
-		if err := c.ShouldBind(&order); err != nil {
+		if err := c.ShouldBind(&data); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error": err.Error(),
 			})
@@ -82,24 +197,52 @@ func CreateOrder(db *gorm.DB) gin.HandlerFunc {
 			return
 		}
 
-		if err := db.Create(&order).Error; err != nil {
+		// Check valid item
+		for _, orderItem := range data.OrderItems {
+			var item model.DrinkItem
+			if err := db.Table(model.DrinkItem{}.TableName()).First(&item, orderItem.ItemID).Error; err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": err.Error(),
+					"data":  orderItem.ItemID,
+				})
+
+				return
+			}
+		}
+
+		// Create order
+		if err := db.Create(&data).Error; err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error": err.Error(),
 			})
 
 			return
+		}
+
+		// Create Order_Items
+		for _, orderItem := range data.OrderItems {
+			orderItem.OrderID = data.ID
+			if err := db.Create(&orderItem).Error; err != nil {
+
+				db.Table(Order{}.TableName()).Where("id = ?", data.ID).Delete(nil)
+
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": err.Error(),
+				})
+
+				return
+			}
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"data": order.ID,
+			"data": data.ID,
 		})
 	}
 }
 
 func ListOrders(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var orders []Orders
-
+		var orders []Order
 		if err := db.Find(&orders).Error; err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error": err.Error(),
